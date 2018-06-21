b73d0e7c0caa5ee6ad11a1ba14e9523c2375792b
buck005 <buck005@163.com>
2018-06-21 14:15:41 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <template>
-  <div id="app">
+  <!-- <div id="app">
     <keep-alive>
       <transition name="fade">
         <router-view></router-view>
       </transition>
     </keep-alive>
+  </div> -->
+
+   <div id="app">
+    <transition :name="transitionName">   
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -13,27 +19,64 @@
   export default {
     name: 'App',
     data: function () {
-      return {
-      }
+       return {
+          transitionName:''
+      } 
     },
-
+     watch: {//使用watch 监听$router的变化
+      $route(to, from) {
+        //如果to索引大于from索引,判断为前进状态,反之则为后退状态
+        if(to.meta.index > from.meta.index){
+          //设置动画名称
+          this.transitionName = 'slide-left';
+        }else{
+          this.transitionName = 'slide-right';
+        }
+      }
+    }
   }
 </script>
 
 <style>
-  #app {
+  /* #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
     margin-top: 60px;
-  }
-  .fade-enter-active, .fade-leave-active {
+  } */
+ /*  .fade-enter-active, .fade-leave-active {
     transition: opacity .5s;
   }
   .fade-enter, .fade-leave-to {
     opacity: 0 !important;
-  }
+  } */
+
+.slide-right-enter-active,
+.slide-right-leave-active,
+.slide-left-enter-active,
+.slide-left-leave-active {
+  will-change: transform;
+  transition: all 500ms;
+  position: absolute;
+}
+.slide-right-enter {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
+.slide-right-leave-active {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-enter {
+  opacity: 0;
+  transform: translate3d(100%, 0, 0);
+}
+.slide-left-leave-active {
+  opacity: 0;
+  transform: translate3d(-100%, 0, 0);
+}
 
 </style>
+
